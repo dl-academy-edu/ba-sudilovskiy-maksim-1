5d4d6ce105e2e442e85a8f39bc1392733ce51d76
maclaren23 <91137538+maclaren23@users.noreply.github.com>
2021-11-15 00:05:56 +0400

    Верстка всех страниц(без адаптива)
    Сделал поправки в доступности сайта и закрывание popup на клавишу Escape.

--- dlk/js/popup.js
+++ dlk/js/popup.js
@@ -6,12 +6,14 @@ function openPopup() {
   const popup = document.querySelector('.popup-contacts');
   const bodyPopup = document.querySelector('.popup-contacts ');
   const bodyPage = document.querySelector('body');
+  const inpName = document.querySelector('#contact-name');
 
-  btnOpen.addEventListener('click', (e) => {
-    e.preventDefault();
+  btnOpen.addEventListener('click', () => {
     popup.classList.add('active');
     bodyPage.style.overflow = 'hidden';
+    inpName.focus();
   });
+
   bodyPopup.addEventListener('click', (e) => {
     const target = e.target;
     if (
@@ -20,10 +22,18 @@ function openPopup() {
     ) {
       popup.classList.remove('active');
       bodyPage.style.overflow = '';
+      btnOpen.focus();
+    }
+  });
+
+  window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' && popup.classList.contains('active')) {
+      popup.classList.remove('active');
+      bodyPage.style.overflow = '';
+      btnOpen.focus();
     }
   });
 }
-
 
 openPopup();
 
